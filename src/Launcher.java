ab6f148a8b93855a882c0c75eb4b37e2442f5537
Denis Baskulin <baskulin@mail.ru>
2023-05-15 16:03:54 +0300

    Implement add function

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,8 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Launcher {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World");
+        int a = 5;
+        int b = 3;
+
+        System.out.println(add(a, b));
+    }
+
+        private static int add(int a, int b) {
+        return a + b;
     }
 }
